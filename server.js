a810a4cc729a3281b64a19e07db4de3a54a959fb
AJW <ajweber102@gmail.com>
2019-07-24 09:06:23 -0400

    Data parsing, router addition, server listener

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-// Initialize dependencies
+// Initialize Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-// Initialize express
+// Initialize Express
 var app = express();
 
-// Establish server port var
+// Establish Server Port
 var PORT = process.env.PORT || 8080;
+
+// Data Parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+
+//Initialize Router
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+//Listen on the server
+app.listen(PORT, function () {
+    console.log('Listening on PORT: ' + PORT);
+});
