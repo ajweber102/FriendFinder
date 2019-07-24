d6d91b47783d5928a8fb6f970c2bc1745a47decb
AJW <ajweber102@gmail.com>
2019-07-24 09:52:47 -0400

    Added mock profiles to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,44 @@
+var friendsArr [
+    {
+        "name": "Bob McBobface",
+        "photo": "C",
+        "scores": ["4","5","5","3","2","5","4","3","3","1"]
+    },
+    {
+        "name": "Billie Jean",
+        "photo": "https://i.pinimg.com/originals/73/fe/b0/73feb0af0a6a9a5c8e9597755a0bbce1.jpg",
+        "scores": ["2","1","4","2","2","5","7","1","1","4"]
+    },
+    {
+        "name": "Nillie Welson",
+        "photo": "https://www.billboard.com/files/media/willie-nelson-farm-aid-2018-live-billboard-1548.jpg",
+        "scores": ["1","1","1","2","5","4","1","4","5","1"]
+    },
+    {
+        "name": "Rob Cob",
+        "photo": "https://www.thegunnysack.com/wp-content/uploads/2018/06/Boiled-Corn-On-The-Cob-Recipe.jpg",
+        "scores": ["4","5","1","1","5","2","1","4","1","1"]
+    },
+    {
+        "name": "Coolguy Sam",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/5/55/Pajama_Sam.png",
+        "scores": ["5","5","5","3","5","5","1","1","2","2"]
+    },
+    {
+        "name": "Roe Jogan",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/5/55/Pajama_Sam.png",
+        "scores": ["3","3","3","3","1","2","3","4","5","5"]
+    },
+    {
+        "name": "Janice Wilson",
+        "photo": "http://www.clipartroo.com/images/76/female-smiley-face-clipart-76618.png",
+        "scores": ["1","1","3","4","2","3","4","2","5","5"]
+    },
+    {
+        "name": "Leslie Knope",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/3/34/Leslie_Knope_%28played_by_Amy_Poehler%29.png",
+        "scores": ["1","3","3","3","5","5","3","2","2","4"]
+    }
+]
+
+module.exports = friendsArr;
