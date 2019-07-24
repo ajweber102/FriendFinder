d3ba3e79d8a66a51603e156c902dfdedc92b1931
AJW <ajweber102@gmail.com>
2019-07-24 15:37:08 -0400

    added routes via get method. formatted friends array.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,19 @@
 //Initialize path in file
 var path = require('path');
+
+// Routes
+module.exports = function(app) {
+    // Homepage route
+    app.get('/', function (req, res) {
+        res.sendFile(path.join(__dirname, '/../public/home.html'));
+    });
+    // Survey page route
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname, '/../public/survey.html'));
+    });
+
+    // Error path -- revert to home
+    app.use(function (req, res) {
+        res.sendFile(path.join(__dirname + '/../public/home.html'));
+    });
+};
