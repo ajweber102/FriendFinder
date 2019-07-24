6fdcaf404dfff948d76e855f55121b52a1082f38
AJW <ajweber102@gmail.com>
2019-07-24 14:46:03 -0400

    Created post function to capture results and parse them

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,3 +5,19 @@ module.exports = function (app) {
       res.json(friends);
   });
 };
+
+
+app.post('/api/friends', function (req, res) {
+    var yourFriend = req.body;
+    var bestMatch = {};
+
+    for(var i = 0; i < yourFriend.scores.length; i++) {
+      if(yourFriend.scores[i] == "1 (Strongly Disagree)") {
+        yourFriend.scores[i] = 1;
+      } else if(yourFriend.scores[i] == "5 (Strongly Agree)") {
+        yourFriend.scores[i] = 5;
+      } else {
+        yourFriend.scores[i] = parseInt(yourFriend.scores[i]);
+      }
+    }
+});
