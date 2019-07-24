c4370076c9ec1885626fe725a21dfd7ee4259a08
AJW <ajweber102@gmail.com>
2019-07-24 08:46:24 -0400

    Initialize dependencies and establish server port

---
+++ server.js
@@ -0,0 +1,10 @@
+// Initialize dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Initialize express
+var app = express();
+
+// Establish server port
+var PORT = process.env.PORT;
